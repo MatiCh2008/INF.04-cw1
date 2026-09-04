cf13e33a7f83d5bf0acfc0a9e23ed823c9e8f0af
Mateusz Chmielewski <mateochmiel2008@gmail.com>
2026-09-04 08:47:34 +0200

    gjh

--- Cw1/src/App.jsx
+++ Cw1/src/App.jsx
@@ -1,14 +1,24 @@
 
-const Kursy = () => {
-    <div>
+const Kursy = (props) => {
+    return <div>
       <h2>Liczba kursów: </h2>
+      {
+        props.dane[1]
+      }
     </div>
 }
 
 const App = () => {
+
+  const kursy = [
+      "Programowanie w C#",
+      "Angular dla początkujących",
+      "Kurs Django"
+  ]
+
   return (
     <div>
-      <Kursy />
+      <Kursy dane={kursy}/>
     </div>
   )
 }
