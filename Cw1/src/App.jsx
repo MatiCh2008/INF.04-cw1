204de66882aff5b70aa5f135c13cf49910533fa3
Mateusz Chmielewski <mateochmiel2008@gmail.com>
2026-09-18 11:49:01 +0200

    Submit początek

--- Cw1/src/App.jsx
+++ Cw1/src/App.jsx
@@ -1,25 +1,38 @@
 import "bootstrap/dist/css/bootstrap.css";
 
+import { use, useState } from "react";
+
 function App() {
+
+  const [courses, setCourses] = useState([
+    'Programowanie w C#',
+    'Angular dla początkujących',
+    'Kurs Django'
+  ]);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submit");
+  }
 
   return (
     <div>
-      <h2>Liczba kursów: 3</h2>
+      <h2>Liczba kursów: {courses.length}</h2>
       <ol>
-        <li>Programowanie w C#</li>
-        <li>Angular dla początkujących</li>
-        <li>Kurs Django</li>        
+        {courses.map(course => <li key={course}>{course}</li>)}
       </ol>
 
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
-          <label for="name-and-surname">Imię i nazwisko:</label>
+          <label htmlFor="name-and-surname">Imię i nazwisko:</label>
           <input className="form-control" type="text" id="name-and-surname" />
         </div>
         <div className="form-group">
-          <label for="course-number">Numer kursu:</label> 
-          <input className="form-control" type="text" id="course-number" />         
-        </div>        
+          <label htmlFor="course-number">Numer kursu:</label> 
+          <input className="form-control" type="number" id="course-number" />         
+        </div>       
+
+        <button className="btn btn-primary">Zapisz do kursu</button> 
       </form>
     </div>
   );
