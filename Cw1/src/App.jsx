b71216f436425c16c2d67d76b176716359576a61
Mateusz Chmielewski <mateochmiel2008@gmail.com>
2026-09-24 15:18:12 +0200

    koniec

--- Cw1/src/App.jsx
+++ Cw1/src/App.jsx
@@ -1,7 +1,29 @@
+import { Button } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
+import { useState } from "react"
+
 const App = () => {
+
+  const [cities, setCities] = useState([
+  "Szczecin",
+  "Gdańsk",
+  "Kraków",
+  "Wrocław",
+  "Poznań"
+  ]);
+
+  const showInConsole = (event) => {
+    console.log(cities);
+  }
+
   return (
     <div>
-      <p>Hello world</p>
+      <h2>Liczba miast: {cities.length}</h2>
+      <ol>
+        {cities.map(city => <li key={city}>{city}</li>)}
+      </ol>
+      <button className="btn btn-primary" onClick={showInConsole}>Pokaż w konsoli</button>
     </div>
   )
 }
